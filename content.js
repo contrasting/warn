7c025ab7309117e9fb6581f769c8a8483f86d70d
oldpilgrim <40671177+oldpilgrim@users.noreply.github.com>
2023-05-13 00:14:58 +0100

    initial commit

---
+++ content.js
@@ -0,0 +1,6 @@
+const wasteTime = confirm("This is a time wasting website. Are you sure you want to visit?");
+
+if (!wasteTime) {
+  // https://stackoverflow.com/questions/32201846/can-an-extension-close-a-tab
+  chrome.runtime.sendMessage({closeThis: true});
+}
