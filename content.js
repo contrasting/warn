637288e70cf011c2fbddd59399b7e5096faf2d0d
oldpilgrim <40671177+oldpilgrim@users.noreply.github.com>
2023-05-13 00:24:46 +0100

    add blocker div

--- content.js
+++ content.js
@@ -1,6 +1,23 @@
-const wasteTime = confirm("This is a time wasting website. Are you sure you want to visit?");
+// first block the view
+const blocker = document.createElement("div");
+blocker.style.width = "100%"
+blocker.style.height = "100%"
+blocker.style.background = "black"
+blocker.style.position = "fixed"
+blocker.style.zIndex = "20000"
+blocker.id = "warn_blocker"
+document.body.prepend(blocker)
 
-if (!wasteTime) {
-  // https://stackoverflow.com/questions/32201846/can-an-extension-close-a-tab
-  chrome.runtime.sendMessage({closeThis: true});
+// wait for blocker to load in
+setTimeout(showConfirm, 50)
+
+function showConfirm() {
+  const wasteTime = confirm("This is a time wasting website. Are you sure you want to visit?");
+
+  if (!wasteTime) {
+    // https://stackoverflow.com/questions/32201846/can-an-extension-close-a-tab
+    chrome.runtime.sendMessage({closeThis: true});
+  } else {
+    document.getElementById("warn_blocker").remove();
+  }
 }
