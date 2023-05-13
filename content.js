280a769a4c6106ea8c818706e4f7cf9f14feca18
oldpilgrim <40671177+oldpilgrim@users.noreply.github.com>
2023-05-13 11:06:31 +0100

    fix removing redundant div

--- content.js
+++ content.js
@@ -3,6 +3,4 @@ const wasteTime = confirm("This is a time wasting website. Are you sure you want
 if (!wasteTime) {
   // https://stackoverflow.com/questions/32201846/can-an-extension-close-a-tab
   chrome.runtime.sendMessage({closeThis: true});
-} else {
-  document.getElementById("warn_blocker").remove();
 }
